949e56ee9cf5c3354009ad5fd606b13829dd6a01
L0615T1C5-216AC-9437 <60281819+L0615T1C5-216AC-9437@users.noreply.github.com>
2020-04-30 15:18:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+### Description
+Basic foundation for a server like chaotic-neutral or .io . with this, you can store player data, login, and whatever you choose.
+To use this plugin, you must fork this repo and add your own code. Should the foundation have an issue, it will be fixed for you too!
+
+
+### Building a Jar
+
+1) fork this repo
+2) download src
+3) run gradlew.bat
+4) go to the plugin folder in cmd. (example: `cd C:\user\one\desk\pluginfolder\`)
+5) type `gradlew jar` and execute (`./gradlew jar` on linux)
+6) done, look for plugin.jar in `pluginfolder\build\libs\`
+
+Note: Highly recommended to use Java 8.
+
+### Installing
+
+Simply place the output jar from the step above in your server's `config/mods` directory and restart the server.
+List your currently installed plugins/mods by running the `mods` command.
+
+### Self Promotion
+Our discord server: http://cn-discord.ddns.net  
+Our game servers:  
+chaotic-neutral.ddns.net:1111  
+chaotic-neutral.ddns.net:2222  
---
+++ src/main/java/core/Main.java
@@ -0,0 +1,50 @@
+package core;
+
+import arc.util.*;
+import mindustry.plugin.Plugin;
+import org.json.JSONObject;
+
+public class Main extends Plugin {
+    String userHomePath = System.getProperty("user.home");
+    String location = userHomePath+"/mind_db/";
+    @Override
+    public void registerServerCommands(CommandHandler handler) {
+        handler.register("make","<fileName>", "makes a json.cn with a basic object", arg -> {
+            JSONObject object = new JSONObject();
+            object.put("Key", "Value");
+            Log.info(byteCode.make(arg[0],object));
+        });
+        handler.register("makedir","<fileName>", "makes a json.cn with a basic object", arg -> {
+            Log.info(byteCode.mkdir(arg[0]));
+        });
+        handler.register("get","<fileName>", "gets everything inside the json.cn", arg -> {
+            JSONObject data = byteCode.get(arg[0]);
+            if (data != null) {
+                StringBuilder builder = new StringBuilder();
+                builder.append("\n");
+                for (String keyStr : data.keySet()) {
+                    Object keyvalue = data.get(keyStr);
+                    //Print key and value
+                    builder.append(keyStr + " : " + keyvalue).append("\n");
+                }
+                Log.info(builder.toString());
+            }
+        });
+        handler.register("putstr","<fileName> <key> <value>", "puts a string in into the json.cn", arg -> {
+            Log.info(byteCode.putStr(arg[0], arg[1], arg[2]));
+        });
+        handler.register("putint", "<fileName> <key> <value>", "puts a integer in into the json.cn", arg -> {
+            int i = Strings.parseInt(arg[2]);
+            Log.info(byteCode.putInt(arg[0], arg[1], i));
+        });
+        handler.register("remove","<fileName> <key>", "puts a string in into the json.cn", arg -> {
+            Log.info(byteCode.remove(arg[0], arg[1]));
+        });
+        handler.register("has","<fileName>", "makes a json.cn with a basic object", arg -> {
+            Log.info("Has `" + location + arg[0] + ".cn` : " + byteCode.has(arg[0]));
+        });
+        handler.register("hasdir","<fileName>", "makes a json.cn with a basic object", arg -> {
+            Log.info("Has `" + userHomePath+"/"+arg[0] + "/` : " + byteCode.hasDir(arg[0]));
+        });
+    }
+}
